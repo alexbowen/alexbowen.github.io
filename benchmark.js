ca393b2f5118e44eb2e411f2bba78d05fec78afa
Alex Bowen <alex.bowen@marks-and-spencer.com>
2014-04-01 10:06:15 +0100

    added doc ready

--- benchmark.js
+++ benchmark.js
@@ -1,113 +1,118 @@
-Storage.prototype.addToArray = function(key, value) {
-  var oldArray = this.getItem(key) ? this.getItem(key).split(',') : [];
-  oldArray.push(value);
-  this.setArray(key, oldArray.join(','));
-}
+$(document).ready(function() {
 
-Storage.prototype.setArray = function(key, str) {
-    return this.setItem(key, str)
-}
-
-Storage.prototype.getArray = function(key) {
-    return JSON.parse(this.getItem(key))
-}
-
-Storage.prototype.getAverage = function (key) {
-  if (!this.getItem(key)) return false;
-
-  var resultsArray = this.getItem(key).split(',');
-
-  var sum = 0;
-
-  for(var i = 0; i < resultsArray.length; i++){
-      sum += parseInt(resultsArray[i]);
+  Storage.prototype.addToArray = function(key, value) {
+    var oldArray = this.getItem(key) ? this.getItem(key).split(',') : [];
+    oldArray.push(value);
+    this.setArray(key, oldArray.join(','));
   }
 
-  return sum/resultsArray.length;
-}
+  Storage.prototype.setArray = function(key, str) {
+      return this.setItem(key, str)
+  }
 
-var drawGraph = function () {
-    $(document).ready(function () { 
-        $('#container').highcharts({
-            chart: {
-                type: 'bar'
-            },
-            title: {
-                text: ''
-            },
-            xAxis: {
-                categories: ['Mechanism']
-            },
-            yAxis: {
-                title: {
-                    text: 'Average time to load jQuery'
-                }
-            },
-            series: [{
-                name: 'Browser Cache',
-                data: [window.localStorage.getAverage('cacheResults')]
-            }, {
-                name: 'BasketJS',
-                data: [window.localStorage.getAverage('basketResults')]
-            }]
-        });
-    });
-}
+  Storage.prototype.getArray = function(key) {
+      return JSON.parse(this.getItem(key))
+  }
 
-var teardown = function() {
-  basket.remove("basket-jquery.js");
-};
+  Storage.prototype.getAverage = function (key) {
 
-var setup = function() {
+    if (!this.getItem(key)) return false;
 
-    var url = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
+    var resultsArray = this.getItem(key).split(',');
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, false);
-    xhr.send();
-    if (xhr.status === 200) {
+    var sum = 0;
 
-      localStorage.setItem("basket-jquery", xhr.responseText);
-
-      var cacheTest = function () {
-
-        var now = new Date().getTime(), xhr = new XMLHttpRequest();
-
-        xhr.open("GET", url);
-        xhr.onreadystatechange = function(xhrEvent) {
-
-          if (xhr.readyState === 4 && xhr.status === 200) {
-
-            var timeCache = xhrEvent.timeStamp - now;
-
-            window.localStorage.addToArray("cacheResults", timeCache);
-
-            drawGraph();
-          }
-        };
-        xhr.send();
-      }
-
-      var basketTest = function () {
-
-        var now = new Date().getTime();
-
-        basket.require({ url: "jquery.js"}).then(function() {
-
-            var endBasket = new Date().getTime(), timeBasket = endBasket - now;
-
-            window.localStorage.addToArray("basketResults", timeBasket);
-
-            drawGraph();
-
-          }, function (error) {
-            console.log('error', error);
-          });
-      }
-
-      basketTest();
-      cacheTest();
+    for(var i = 0; i < resultsArray.length; i++){
+        sum += parseInt(resultsArray[i]);
     }
-};
 
-setup();
+    return sum/resultsArray.length;
+  }
+
+  var drawGraph = function () {
+      $(document).ready(function () { 
+          $('#container').highcharts({
+              chart: {
+                  type: 'bar'
+              },
+              title: {
+                  text: ''
+              },
+              xAxis: {
+                  categories: ['Mechanism']
+              },
+              yAxis: {
+                  title: {
+                      text: 'Average time to load jQuery'
+                  }
+              },
+              series: [{
+                  name: 'Browser Cache',
+                  data: [window.localStorage.getAverage('cacheResults')]
+              }, {
+                  name: 'BasketJS',
+                  data: [window.localStorage.getAverage('basketResults')]
+              }]
+          });
+      });
+  }
+
+  var teardown = function() {
+    basket.remove("basket-jquery.js");
+  };
+
+  var setup = function() {
+
+      var url = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
+
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url, false);
+      xhr.send();
+      if (xhr.status === 200) {
+
+        localStorage.setItem("basket-jquery", xhr.responseText);
+
+        var cacheTest = function () {
+
+          var now = new Date().getTime(), xhr = new XMLHttpRequest();
+
+          xhr.open("GET", url);
+          xhr.onreadystatechange = function(xhrEvent) {
+
+            if (xhr.readyState === 4 && xhr.status === 200) {
+
+              var timeCache = xhrEvent.timeStamp - now;
+
+              window.localStorage.addToArray("cacheResults", timeCache);
+
+              drawGraph();
+            }
+          };
+          xhr.send();
+        }
+
+        var basketTest = function () {
+
+          var now = new Date().getTime();
+
+          basket.require({ url: "jquery.js"}).then(function() {
+
+              var endBasket = new Date().getTime(), timeBasket = endBasket - now;
+
+              window.localStorage.addToArray("basketResults", timeBasket);
+
+              drawGraph();
+
+            }, function (error) {
+              console.log('error', error);
+            });
+        }
+
+        basketTest();
+        cacheTest();
+      }
+  };
+
+  setup();
+
+});
