4c0ce3792727851447fa344c20dc59757abc97fa
Alex Bowen <alex.bowen@marks-and-spencer.com>
2014-04-01 16:53:31 +0100

    started coding standards

--- benchmark.js
+++ benchmark.js
@@ -1,118 +1,115 @@
-$(document).ready(function() {
+Storage.prototype.addToArray = function(key, value) {
+  var oldArray = this.getItem(key) ? this.getItem(key).split(',') : [];
+  oldArray.push(value);
+  this.setItem(key, oldArray.join(','));
+};
 
-  Storage.prototype.addToArray = function(key, value) {
-    var oldArray = this.getItem(key) ? this.getItem(key).split(',') : [];
-    oldArray.push(value);
-    this.setArray(key, oldArray.join(','));
+Storage.prototype.getArray = function(key) {
+    return JSON.parse(this.getItem(key))
+};
+
+Storage.prototype.getAverage = function (key) {
+
+  if (!this.getItem(key)) return false;
+
+  var resultsArray = this.getItem(key).split(',');
+
+  var sum = 0;
+
+  for(var i = 0; i < resultsArray.length; i++){
+      sum += parseInt(resultsArray[i]);
   }
 
-  Storage.prototype.setArray = function(key, str) {
-      return this.setItem(key, str)
-  }
+  return sum/resultsArray.length;
+};
 
-  Storage.prototype.getArray = function(key) {
-      return JSON.parse(this.getItem(key))
-  }
+var drawGraph = function () {
+    $(document).ready(function () { 
+        $('#container').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: ''
+            },
+            xAxis: {
+                categories: ['Mechanism']
+            },
+            yAxis: {
+                title: {
+                    text: 'Average time to load jQuery'
+                }
+            },
+            series: [{
+                name: 'Browser Cache',
+                data: [window.localStorage.getAverage('cacheResults')]
+            }, {
+                name: 'BasketJS',
+                data: [window.localStorage.getAverage('basketResults')]
+            }]
+        });
+    });
+};
 
-  Storage.prototype.getAverage = function (key) {
+var teardown = function() {
+  basket.remove("basket-jquery.js");
+};
 
-    if (!this.getItem(key)) return false;
+var getTime = function () {
+  return window.performance.webkitNow ? window.performance.webkitNow() : new Date().getTime();       
+};
 
-    var resultsArray = this.getItem(key).split(',');
+var setup = function() {
 
-    var sum = 0;
+    var url = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
 
-    for(var i = 0; i < resultsArray.length; i++){
-        sum += parseInt(resultsArray[i]);
-    }
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    xhr.send();
+    if (xhr.status === 200) {
 
-    return sum/resultsArray.length;
-  }
+      localStorage.setItem("basket-jquery", xhr.responseText);
 
-  var drawGraph = function () {
-      $(document).ready(function () { 
-          $('#container').highcharts({
-              chart: {
-                  type: 'bar'
-              },
-              title: {
-                  text: ''
-              },
-              xAxis: {
-                  categories: ['Mechanism']
-              },
-              yAxis: {
-                  title: {
-                      text: 'Average time to load jQuery'
-                  }
-              },
-              series: [{
-                  name: 'Browser Cache',
-                  data: [window.localStorage.getAverage('cacheResults')]
-              }, {
-                  name: 'BasketJS',
-                  data: [window.localStorage.getAverage('basketResults')]
-              }]
-          });
-      });
-  }
+      var cacheTest = function () {
 
-  var teardown = function() {
-    basket.remove("basket-jquery.js");
-  };
+        var now = getTime();
+        var xhr = new XMLHttpRequest();
 
-  var setup = function() {
+        xhr.open("GET", url);
+        xhr.onreadystatechange = function(xhrEvent) {
 
-      var url = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
+          if (xhr.readyState === 4 && xhr.status === 200) {
 
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", url, false);
-      xhr.send();
-      if (xhr.status === 200) {
+            var timeCache = getTime() - now;
 
-        localStorage.setItem("basket-jquery", xhr.responseText);
+            window.localStorage.addToArray("cacheResults", timeCache);
 
-        var cacheTest = function () {
-
-          var now = new Date().getTime(), xhr = new XMLHttpRequest();
-
-          xhr.open("GET", url);
-          xhr.onreadystatechange = function(xhrEvent) {
-
-            if (xhr.readyState === 4 && xhr.status === 200) {
-
-              var timeCache = xhrEvent.timeStamp - now;
-
-              window.localStorage.addToArray("cacheResults", timeCache);
-
-              drawGraph();
-            }
-          };
-          xhr.send();
-        }
-
-        var basketTest = function () {
-
-          var now = new Date().getTime();
-
-          basket.require({ url: "jquery.js"}).then(function() {
-
-              var endBasket = new Date().getTime(), timeBasket = endBasket - now;
-
-              window.localStorage.addToArray("basketResults", timeBasket);
-
-              drawGraph();
-
-            }, function (error) {
-              console.log('error', error);
-            });
-        }
-
-        basketTest();
-        cacheTest();
+            drawGraph();
+          }
+        };
+        xhr.send();
       }
-  };
 
-  setup();
+      var basketTest = function () {
 
-});
+        var now = getTime();
+
+        basket.require({ url: "jquery.js"}).then(function() {
+
+            var endBasket = getTime(), timeBasket = endBasket - now;
+
+            window.localStorage.addToArray("basketResults", timeBasket);
+
+            drawGraph();
+
+          }, function (error) {
+            console.log('error', error);
+          });
+      }
+
+      basketTest();
+      cacheTest();
+    }
+};
+
+setup();
---
+++ tests/newDevTests.js
@@ -0,0 +1,114 @@
+//compare these two code snippets
+
+//---------------------------------------------
+
+var person = (function () {
+ 
+    var name = "Alex Bowen",
+        welcome  = "Hey there!";
+
+    function logName() {
+        console.log( "Name:" + name );
+    };
+
+    function setName( nameString ) {
+        name = nameString;
+    };
+
+    function getName() {
+        logName();
+    };
+
+    return {
+        setName: setName,
+        getName: getName
+    };
+
+})();
+
+person.logName(); //what does this do?
+
+//---------------------------------------------
+
+var person = function () {};
+
+person.prototype = {
+
+    name : "Alex Bowen",
+
+    welcome  : "Hey there!",
+
+    setName : function ( nameString ) {
+        this.name = nameString;
+    },
+
+    getName : function () {
+        logName();
+    },
+
+    logName() {
+        console.log( "Name:" + this.name );
+    }
+}
+
+person.logName();  //what does this do?
+
+//---------------------------------------------
+
+//tell me what you think about this code snippet
+
+for (var i = 0; i < rows; i++) {
+    $tr = $('<tr></tr>');
+    for (var j = 0; j < this.data.length; j++) {
+        $tr.append('<td>' + this.data[j]['id'] + '</td>');
+    }
+    $tr.appendTo($tbody);
+}
+
+//tell me where you might see this:
+
+return {
+    restrict: 'A',
+    require: '^ngCity',
+    scope: {
+      ngCity: '@'
+    },
+    link: function(scope, element, attrs) {
+      // code
+    }
+}
+
+//tell me about the following code snippets
+
+function foo() {
+    var bar = new LargeObject();
+    bar.someCall();
+}
+
+var b = foo();
+
+//---------------------------------------------
+
+function foo() {
+    var bar = new LargeObject();
+    bar.someCall();
+    return bar;
+}
+
+var b = foo();
+
+//tell me what you think about this code snippet
+
+Array.prototype.join = function (array1, array2) {
+    return array1 + array2;
+}
+
+//QUESTIONs
+
+//1. what is event delegation?
+
+//2. what is jQuery and what is it not?
+
+//3. describe what the difference between apply and call is?
+
+//4. would you consider it best to load CSS or javascript first and why?
